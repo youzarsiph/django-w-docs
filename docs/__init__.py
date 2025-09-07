db92f8f217d5cdb4241472cb3f769ea0d9153452
Yousef Abu Shanab <93343012+youzarsiph@users.noreply.github.com>
2025-09-06 23:14:31 -0700

    update cms app label

--- docs/__init__.py
+++ docs/__init__.py
@@ -1,1 +1,1 @@
-"""Documentation"""
+"""Django Docs: A reusable documentation app powered by Python, Django, DRF, Wagtail CMS, TailwindCSS and DaisyUI."""
--- docs/cms/apps.py
+++ docs/cms/apps.py
@@ -8,4 +8,5 @@ class CMSConfig(AppConfig):
     """App configuration for docs.cms"""
 
     name = "docs.cms"
+    label = "docs_cms"
     default_auto_field = "django.db.models.BigAutoField"
